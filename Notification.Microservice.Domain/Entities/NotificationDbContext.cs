eef8564b4b03b51219a8b7bec3c5634e23b0bf43
CS-Programmer254 <stanleigh3oduor@gmail.com>
2024-06-23 09:47:22 +0300

    Notification Db Context Done

--- Notification.Microservice.Domain/Entities/NotificationDbContext.cs
+++ Notification.Microservice.Domain/Entities/NotificationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace Notification.Microservice.Domain.Entities
 {
-    internal class NotificationDbContext
+    public class NotificationDbContext:DbContext
     {
+        public NotificationDbContext(DbContextOptions<NotificationDbContext>options):base(options)
+        {
+            
+        }
+        public DbSet<Notifications> notifications { get; set; }
     }
 }
